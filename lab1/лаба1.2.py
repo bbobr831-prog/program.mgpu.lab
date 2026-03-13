87cc6376be40763d91b7fff187a84dfb92e47427
bbobr831-prog <bbobr831@gmail.com>
2026-03-13 11:04:27 +0300

    Add files via upload

---
+++ lab1/лаба1.2.py
@@ -0,0 +1,9 @@
+# Задание task_01_02.
+# Выполнил Бобр Богдан 
+# Группа: ЦИБ-251
+print("Пример вывода:")
+x=int(input("x="))
+y=int(input("y="))
+z= int(input("z="))
+result=(x+y+z)/20 
+print(round(result,2))
