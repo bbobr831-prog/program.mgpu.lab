632d4bf0406a57e87456483dcde2f4fc37374d93
bbobr831-prog <bbobr831@gmail.com>
2026-03-27 11:03:20 +0300

    Delete lab1/лаба1.2.py

--- lab1/лаба1.2.py
+++
@@ -1,9 +0,0 @@
-# Задание task_01_02.
-# Выполнил Бобр Богдан 
-# Группа: ЦИБ-251
-print("Пример вывода:")
-x=int(input("x="))
-y=int(input("y="))
-z= int(input("z="))
-result=(x+y+z)/20 
-print(round(result,2))
